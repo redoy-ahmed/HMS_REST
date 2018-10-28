9858b02d389de7a8319ce1529df6d1f19276b4b3
redoy <redoyahmed@gmail.com>
2018-10-28 22:40:02 +0600

    commit from home

--- src/main/java/com/hospital/Controller/LogIn/LogInController.java
+++ src/main/java/com/hospital/Controller/LogIn/LogInController.java
@@ -6,9 +6,10 @@ import com.hospital.Security.Token.ITokenGenerator;
 import com.hospital.Service.Interface.ILogInService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.ws.rs.Path;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LogInController {
--- src/main/java/com/hospital/Security/jwt/JwtTokenFilter.java
+++ src/main/java/com/hospital/Security/jwt/JwtTokenFilter.java
@@ -1,17 +1,11 @@
 package com.hospital.Security.jwt;
 
 import io.jsonwebtoken.Jwts;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
-import org.springframework.stereotype.Component;
 
 import javax.crypto.spec.SecretKeySpec;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
-import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
