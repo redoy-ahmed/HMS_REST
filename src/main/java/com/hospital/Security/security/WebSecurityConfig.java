d70214443de4ea6cbaa08ad0ac6c6cc261454a37
redoy.ahmed <redoyahmed@gmail.com>
2018-10-30 10:24:08 +0600

    Some Code Changed

--- src/main/java/com/hospital/Security/security/WebSecurityConfig.java
+++ src/main/java/com/hospital/Security/security/WebSecurityConfig.java
@@ -15,6 +15,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
                 .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/login").permitAll()
                 .antMatchers(HttpMethod.POST, "/users").permitAll()
                 .anyRequest().authenticated()
                 .and()
